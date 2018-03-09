c68289409cfc2689e115a9a25f760b3fe5e6fbc7
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2018-03-08 17:39:08 -0800

    streaming from csv works

--- app/controllers/StreamController.scala
+++ app/controllers/StreamController.scala
@@ -1,12 +1,13 @@
 package controllers
 
 import javax.inject._
+import java.nio.file.{Paths, Files} 
+
 import play.api._
 import play.api.mvc._
 import play.api.http._
 import akka.stream.scaladsl._
 import akka.util._
-
 
 /**
 * Very simple endless streaming example for speed testing.
@@ -17,7 +18,8 @@ class StreamController @Inject(
   ) (cc: ControllerComponents) extends AbstractController(cc) {
 
   def chunkedFromSource() = Action { 
-    val source = Source.repeat("t" * 1024 )
+    val source = FileIO.fromPath(Paths.get("10002070.csv"))
+    // val source = Source.repeat("t" * 1024 )
     Ok.chunked(source).as("text/csv")
   }
 }
--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ scalaVersion := "2.12.3"
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.4.2"
+libraryDependencies += "com.lightbend.akka" %% "akka-stream-alpakka-csv" % "0.8"
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.example.controllers._"
