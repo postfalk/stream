2993245077eede89f6fe38260fdc60846efeb71f
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2018-03-13 14:00:24 -0700

    taking apart csv and putting it back together

--- app/controllers/StreamController.scala
+++ app/controllers/StreamController.scala
@@ -1,13 +1,23 @@
 package controllers
 
 import javax.inject._
-import java.nio.file.{Paths, Files} 
+import java.nio.file.{Files, Paths} 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
 
 import play.api._
 import play.api.mvc._
 import play.api.http._
+import play.api.libs.iteratee._
+
+import akka.stream._
+import akka.stream.stage._
+
 import akka.stream.scaladsl._
 import akka.util._
+import akka.actor.ActorSystem
+
+import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
 
 /**
 * Very simple endless streaming example for speed testing.
@@ -18,8 +28,51 @@ class StreamController @Inject(
   ) (cc: ControllerComponents) extends AbstractController(cc) {
 
   def chunkedFromSource() = Action { 
+
+    implicit val system = ActorSystem("Stream")
+    implicit val materializer = ActorMaterializer()
+
+    // def parseline[List[ByteString]] = (In) immutable.Seq[Out]) {In}
+
+    val flow = Sink.fold[ByteString, ByteString](ByteString(""))(_ ++ _)
+
     val source = FileIO.fromPath(Paths.get("10002070.csv"))
-    // val source = Source.repeat("t" * 1024 )
-    Ok.chunked(source).as("text/csv")
+      .via(CsvParsing.lineScanner()).mapConcat(identity) //.mapConcat(identity)
+
+
+      // .fold[List[ByteString]](List())(_ ++ _)
+
+
+      //.via(CsvToMap.withHeaders(
+      //  "measurement", "variable", "year", "month", "value"))
+
+
+    source.runWith(Sink.foreach(println(_)))
+
+    // val enumerator() : Enumerator[String] = Enumerator.enumerate(source)
+    // val sink = Sink.ignore //foreach[String](println(_))
+    // val runnable: RunnableGraph[None] = source.toMat(sink)(Keep.right)
+    // val print: Future[None] = runnable.run() */
+    // Ok.chunked(source).as("text/csv") */
+    // implicit val system = ActorSystem("Stream")
+    // implicit val materializer = ActorMaterializer()
+
+    // val source = Source(1 to 10)
+    // val sink = Sink.fold[Int, Int](0)(_ + _)
+
+    // connect the Source to the Sink, obtaining a RunnableGraph
+    // val runnable: RunnableGraph[Future[Int]] = source.toMat(sink)(Keep.right)
+
+    // materialize the flow and get the value of the FoldSink
+    // val duration = Duration(5, "seconds")
+    // val sum: Future[Int] = runnable.run()
+    // val res = Await.result(sum, duration)
+    // println(res)
+
+
+    // val enumerator: Enumerator[String] = Enumeratee(source)
+
+    Ok.chunked(source).as("text/plain")
+    // Ok("hello")
   }
 }
--- build.sbt
+++ build.sbt
@@ -9,7 +9,9 @@ scalaVersion := "2.12.3"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
-libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.4.2"
+libraryDependencies += "com.typesafe.play" %% "play-iteratees" % "2.6.1"
+libraryDependencies += "com.typesafe.play" %% "play-iteratees-reactive-streams" % "2.6.1"
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.5"
 libraryDependencies += "com.lightbend.akka" %% "akka-stream-alpakka-csv" % "0.8"
 
 // Adds additional packages into Twirl
