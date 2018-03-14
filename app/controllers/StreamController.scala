f5c921c36fc1449dc626c4717735b2bf157ea44e
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2018-03-14 15:32:40 -0700

    simple filter works

--- app/controllers/StreamController.scala
+++ app/controllers/StreamController.scala
@@ -2,8 +2,6 @@ package controllers
 
 import javax.inject._
 import java.nio.file.{Files, Paths} 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration.Duration
 
 import play.api._
 import play.api.mvc._
@@ -12,67 +10,76 @@ import play.api.libs.iteratee._
 
 import akka.stream._
 import akka.stream.stage._
-
 import akka.stream.scaladsl._
 import akka.util._
-import akka.actor.ActorSystem
 
 import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
 
 /**
-* Very simple endless streaming example for speed testing.
-* Reaches 130 Mb/s on my MacBook Pro
-*/
-
+ * Very simple endless streaming example for speed testing.
+ * Reaches 130 Mb/s on my MacBook Pro
+ */
 class StreamController @Inject(
   ) (cc: ControllerComponents) extends AbstractController(cc) {
 
-  def chunkedFromSource() = Action { 
+  /**
+   * A play view that streams CSV data from file to download
+   * and applying filters.
+   */
+  def chunkedFromSource() = Action {
 
-    implicit val system = ActorSystem("Stream")
-    implicit val materializer = ActorMaterializer()
+    implicit request: Request[AnyContent] =>
 
-    // def parseline[List[ByteString]] = (In) immutable.Seq[Out]) {In}
+    val params = request.queryString
 
-    val flow = Sink.fold[ByteString, ByteString](ByteString(""))(_ ++ _)
+    /**
+     * Extract query parameters from request for further processing
+     */
+    def getValues(in: Map[String, Seq[String]], key: String) : Seq[String] = {
+      val values = in.get(key)
+      values match {
+        case Some(values) => values
+        case None => Seq()
+      }
+    }
+  
+    /** 
+     * Normalize query parameters in order to accept both ways to represent
+     * lists: ?list=item1,item2 and ?list=item1&list=item2 
+     */
+    def normalize(in: Seq[String]): Seq[String] = {
+      in.foldLeft(Seq[String]()){ _ ++ _.split(",") }
+    }
+
+    /**
+     * Filter function to filter stream by query parameters
+     */
+    def myFilter(in: List[ByteString], req: Request[AnyContent]) = {
+      val params = req.queryString
+      val values = getValues(params, "measurements")
+      // normalize(values).foreach(println(_))
+      // println(in(0) == ByteString("max"))
+      in(0) == ByteString("max")
+    }
+
+    /**
+     * Set request before passing function to flow
+     */
+    def filterFunction(in: List[ByteString]) = myFilter(in: List[ByteString], request)
+
+    /**
+     * Format CSV output stream
+     */
+    def formatCsvLine(lst: List[ByteString]): ByteString = { 
+      lst.reduce(_ ++ ByteString(",") ++ _) ++ ByteString("\n") 
+    }
 
     val source = FileIO.fromPath(Paths.get("10002070.csv"))
-      .via(CsvParsing.lineScanner()).mapConcat(identity) //.mapConcat(identity)
-
-
-      // .fold[List[ByteString]](List())(_ ++ _)
-
-
-      //.via(CsvToMap.withHeaders(
-      //  "measurement", "variable", "year", "month", "value"))
-
-
-    source.runWith(Sink.foreach(println(_)))
-
-    // val enumerator() : Enumerator[String] = Enumerator.enumerate(source)
-    // val sink = Sink.ignore //foreach[String](println(_))
-    // val runnable: RunnableGraph[None] = source.toMat(sink)(Keep.right)
-    // val print: Future[None] = runnable.run() */
-    // Ok.chunked(source).as("text/csv") */
-    // implicit val system = ActorSystem("Stream")
-    // implicit val materializer = ActorMaterializer()
-
-    // val source = Source(1 to 10)
-    // val sink = Sink.fold[Int, Int](0)(_ + _)
-
-    // connect the Source to the Sink, obtaining a RunnableGraph
-    // val runnable: RunnableGraph[Future[Int]] = source.toMat(sink)(Keep.right)
-
-    // materialize the flow and get the value of the FoldSink
-    // val duration = Duration(5, "seconds")
-    // val sum: Future[Int] = runnable.run()
-    // val res = Await.result(sum, duration)
-    // println(res)
-
-
-    // val enumerator: Enumerator[String] = Enumeratee(source)
+      .via(CsvParsing.lineScanner())
+      .filter(filterFunction)
+      .map(formatCsvLine)
 
     Ok.chunked(source).as("text/plain")
-    // Ok("hello")
   }
+
 }
