c40c3350b0545c99396a73180199032ffbcc58f3
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2018-03-20 15:15:26 -0700

    implement basic testing

--- app/controllers/StreamController.scala
+++ app/controllers/StreamController.scala
@@ -22,6 +22,57 @@ class StreamController @Inject(
   ) (cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
+   * Filter function to filter stream by query parameters
+   */
+  def myFilter(
+    in: List[ByteString], 
+    query: List[Seq[ByteString]]): Boolean =
+  {
+    (query(0).isEmpty || query(0).foldLeft(false) { _  || in(0) == _}) &&
+    (query(1).isEmpty || query(1).foldLeft(false) { _  || in(1) == _}) &&
+    (query(2).isEmpty || query(2).foldLeft(false) { _  || in(2) == _}) &&
+    (query(3).isEmpty || query(3).foldLeft(false) { _  || in(3) == _})
+  }
+
+  /** 
+   * 1. Normalize query parameters in order to accept both ways to represent
+   * lists: ?list=item1,item2 and ?list=item1&list=item2
+   * 2. Set default values if query parameter is empty
+   * 3. Convert to ByteString in accordance with Akka
+   */
+  def normalize(in: Seq[String]): Seq[ByteString] = {
+    in.foldLeft(Seq[ByteString]()) { _ ++ _.split(",").map(ByteString(_)) }
+  }
+
+  /**
+   * Extract query parameters from request by keyword for further processing
+   */
+  def getValues(key: String, in: Map[String, Seq[String]]) : Seq[ByteString] = {
+    val values = in.get(key)
+    values match {
+      case Some(values) => normalize(values)
+      case None => Seq()
+    }
+  }
+
+  /**
+   * Create a list of query values to be applied by the filter function.
+   * They are currently mapped by position in the list
+   */
+  def getFilterList(
+    in: Request[AnyContent],
+    keys: List[String]): List[Seq[ByteString]] = {
+      keys.map(getValues(_, in.queryString))
+  }
+
+  /**
+   * Format CSV output stream
+   */
+  def formatCsvLine(lst: List[ByteString]): ByteString = { 
+    lst.reduce(_ ++ ByteString(",") ++ _) ++ ByteString("\n") 
+  }
+
+  /**
    * A play view that streams CSV data from file to download
    * and applying filters.
    */
@@ -30,69 +81,19 @@ class StreamController @Inject(
     implicit request: Request[AnyContent] =>
 
     /**
-     * Keywords that can be used for filtering
+     * Keywords that can be used for filtering, they will be matched by 
+     * position
      */
     val keywords = List(
       "measurements", "variables", "years", "months")
 
-    /** 
-     * 1. Normalize query parameters in order to accept both ways to represent
-     * lists: ?list=item1,item2 and ?list=item1&list=item2
-     * 2. Set default values if query parameter is empty
-     * 3. Convert to ByteString in accordance with Akka
-     */
-    def normalize(in: Seq[String]): Seq[ByteString] = {
-      in.foldLeft(Seq[ByteString]()) { _ ++ _.split(",").map(ByteString(_)) }
-    }
-
-    /**
-     * Extract query parameters from request by keyword for further processing
-     */
-    def getValues(key: String, in: Map[String, Seq[String]]) : Seq[ByteString] = {
-      val values = in.get(key)
-      values match {
-        case Some(values) => normalize(values)
-        case None => Seq()
-      }
-    }
-
-    /**
-     * Create a list of query values to be applied by the filter function.
-     * They are currently mapped by position in the list
-     */
-    def getFilterList(
-      in: Request[AnyContent],
-      keys: List[String] = keywords): List[Seq[ByteString]] = {
-        keys.map(getValues(_, in.queryString))
-    }
-
     val filterList = getFilterList(request, keywords)
 
     /**
-     * Filter function to filter stream by query parameters
-     */
-    def myFilter(
-      in: List[ByteString], 
-      query: List[Seq[ByteString]]): Boolean = 
-    { 
-      (query(0).isEmpty || query(0).foldLeft(false) { _  || in(0) == _}) &&
-      (query(1).isEmpty || query(1).foldLeft(false) { _  || in(1) == _}) &&
-      (query(2).isEmpty || query(2).foldLeft(false) { _  || in(2) == _}) &&
-      (query(3).isEmpty || query(3).foldLeft(false) { _  || in(3) == _})
-    }
-
-    /**
      * Set request before passing function to flow
-     */
+      */
     def filterFunction(in: List[ByteString]): Boolean = 
       myFilter(in: List[ByteString], filterList)
-
-    /**
-     * Format CSV output stream
-     */
-    def formatCsvLine(lst: List[ByteString]): ByteString = { 
-      lst.reduce(_ ++ ByteString(",") ++ _) ++ ByteString("\n") 
-    }
 
     /**
      * Build flow as source
---
+++ test/controllers/StreamControllerSpec.scala
@@ -0,0 +1,59 @@
+package controllers
+
+import scala.concurrent.duration.Duration
+
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.mvc._
+
+import scala.concurrent._
+import akka.stream.Materializer
+import akka.util._
+
+
+class StreamControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting with Results {
+
+  implicit lazy val materializer: Materializer = app.materializer
+
+  "StreamController GET /stream/" should {
+
+    "return Ok and text/plain" in {
+      val controller = new StreamController(stubControllerComponents())
+      val res = controller.chunkedFromSource()
+        .apply(FakeRequest(GET, "/stream/"))
+      status(res) mustBe OK
+      contentType(res) mustBe Some("text/plain")
+    }
+
+    "should be csv" in {
+      val controller = new StreamController(stubControllerComponents())
+      val res = controller.chunkedFromSource()
+        .apply(FakeRequest(GET, "/stream/"))
+      val content = contentAsString(res)
+      val lines = content.split("\n")
+      lines.length must equal (9504)
+      lines.foreach((item) => { 
+        item.split(",").length must equal (5)
+      })
+    }
+  }
+
+  "StreamController myFilter" should {
+    "filter properly" in {
+      val controller = new StreamController(stubControllerComponents())
+      val query = List(
+        Seq(ByteString("min")), Seq[ByteString](), Seq[ByteString](), 
+        Seq[ByteString]()
+      )
+      var in = "min, estimated, 1980, 2, 1.0"
+        .split(",").map(ByteString(_)).toList
+      controller.myFilter(in, query) must be (true)
+      in = "max, estimated, 1980, 2, 1.0"
+        .split(",").map(ByteString(_)).toList
+      controller.myFilter(in, query) must be (false)
+    }
+
+  }
+}
