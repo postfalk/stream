84d81ae4456fc6a0741b386175e1eedfe8033e17
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2018-03-23 15:05:07 -0700

    add explicit graph

--- app/controllers/StreamController.scala
+++ app/controllers/StreamController.scala
@@ -9,6 +9,8 @@ import play.api.mvc._
 import play.api.http._
 import play.api.libs.iteratee._
 
+import akka.NotUsed
+import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.stage._
 import akka.stream.scaladsl._
@@ -31,15 +33,17 @@ class StreamController @Inject(
   {
     (0 until 4).foldLeft(true) { 
       (agg, i) => {
-        if (agg == false) {return false}
-        agg && (query(i).isEmpty || query(i).foldLeft(false) { _ || in(i) == _ })
+        // exit early once agg false, not sure whether that makes sense
+        if (agg == false) { return false }
+        agg && 
+          (query(i).isEmpty || query(i).foldLeft(false) { _ || in(i) == _ })
       }
     }
   }
 
   /** 
    * 1. Normalize query parameters in order to accept both ways to represent
-   * lists: ?list=item1,item2 and ?list=item1&list=item2
+   * lists in urls: ?list=item1,item2 and ?list=item1&list=item2
    * 2. Set default values if query parameter is empty
    * 3. Convert to ByteString in accordance with Akka
    */
@@ -82,6 +86,8 @@ class StreamController @Inject(
   def chunkedFromSource() = Action {
 
     implicit request: Request[AnyContent] =>
+    implicit val system = ActorSystem("Test")
+    implicit val materializer = ActorMaterializer() 
 
     /**
      * Keywords that can be used for filtering, they will be matched by 
@@ -106,10 +112,24 @@ class StreamController @Inject(
       .filter(filterFunction)
       .map(formatCsvLine)
 
+    val src = GraphDSL.create() { 
+      implicit builder: GraphDSL.Builder[NotUsed] => 
+
+        import GraphDSL.Implicits._
+        
+        val sink = Sink.foreach[ByteString](println(_))
+        val f = Flow[ByteString].map((item) => { item  })
+        val stream = source ~> f
+    
+      SourceShape(stream.outlet)
+    }
+
+    val new_source = Source.fromGraph(src)
+
     /**
      * Sink flow to chunked HTTP response
      */
-    Ok.chunked(source).as("text/plain")
+    Ok.chunked(new_source).as("text/plain")
   }
 
 }
