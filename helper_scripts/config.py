9696c1304bd3b80d5cad3e78898069be69d8289b
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2020-04-13 18:34:14 -0700

    fix CORS

--- helper_scripts/config.py
+++ helper_scripts/config.py
@@ -1,10 +1,12 @@
 import os
 
+
 BASE_DIR = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..', '..', 'data'))
 ALL_YEAR_DIR = os.path.join(BASE_DIR, 'nhd_ffm_predictions')
 WYT_DIR = os.path.join(BASE_DIR, 'nhd_ffm_predictions_wyt')
 OUTPUT_DIRECTORY = os.path.join(BASE_DIR, 'ffm')
+
 
 UNIT_DIC = {
     'fa_mag': 'cfs',
